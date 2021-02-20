a4cb4b60d775cae8fd444073737c1aa01df6e54b
felipeoli7eira <me.felipeoliveira@gmail.com>
2021-02-20 13:16:05 -0300

    05.011

--- 05-banco-de-dados-com-pdo/05.12-removendo-registro-ativo/index.php
+++ 05-banco-de-dados-com-pdo/05.12-removendo-registro-ativo/index.php
@@ -9,12 +9,9 @@ require __DIR__ . "/../source/autoload.php";
  */
 fullStackPHPClassSession("destroy", __LINE__);
 
-$model = new \Source\Models\UserModel();
+$model = new \Source\Models\User();
 
-$user = $model->load(5);
-if ($user) {
-    $user->destroy();
-}
+$user = $model->load(2);
 
 var_dump($user);
 
@@ -22,16 +19,4 @@ var_dump($user);
  * [ model destroy ] Deletar em cadeia
  */
 fullStackPHPClassSession("model destroy", __LINE__);
-
-$list = $model->all(100, 30);
-
-if ($list) {
-    /** @var \Source\Models\UserModel $user */
-    foreach ($list as $user) {
-        var_dump($user);
-        $user->destroy();
-    }
-}
-
-var_dump($list);
 
